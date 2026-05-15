9ce03995142c915c6ec9a685292a44dceeb6ffc6
jinhwan1030 <saewoo_@naver.com>
2026-05-15 18:02:14 +0900

    데모 포트폴리오 페이지

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'jhwan.dev';
+export const SITE_DESCRIPTION = '기획자이자 개발자 정진환의 포트폴리오 & 블로그';
