b34985d0c4bae63ad4442cf0141fb1ae1839448a
jinhwan1030 <saewoo_@naver.com>
2026-06-10 11:53:50 +0900

    [design] refine jhwan.dev developer hub UI

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'jhwan.dev';
-export const SITE_DESCRIPTION = '기획자이자 개발자 정진환의 포트폴리오 & 블로그';
+export const SITE_DESCRIPTION = '운영 중인 제품, 홈랩 인프라, 개발 기록을 정리하는 정진환의 Developer Hub';
