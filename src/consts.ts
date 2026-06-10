b20135956a3318550154cd5dbd8c8761c18ae88f
jinhwan1030 <saewoo_@naver.com>
2026-06-10 15:06:59 +0900

    [design] simplify portfolio language and highlights

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'jhwan.dev';
-export const SITE_DESCRIPTION = '운영 중인 제품, 홈랩 인프라, 개발 기록을 정리하는 정진환의 Developer Hub';
+export const SITE_DESCRIPTION = '작은 서비스를 만들고 운영하며 배운 것을 기록하는 정진환의 개인 사이트';
